0b82ede47ea37243f028ccecae39c20e8e5c37d9
The Zuraiz <thezuraiz@gmail.com>
2024-08-17 18:52:01 +0500

    Fix Bugs and Add Save Data

--- lib/data/repositories/user/user_repository.dart
+++ lib/data/repositories/user/user_repository.dart
@@ -1,26 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness_scout/features/personalization/model/user_model.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-class UserRepository extends GetxController{
+import '../../../utils/exceptions/firebase_exception.dart';
+import '../../../utils/exceptions/format_exception.dart';
+
+class UserRepository extends GetxController {
   // --- Variables
   static UserRepository get instance => Get.find();
 
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  /// --- Function to save user data to Firestore
-  saveUserRecord(Map<String,String> user)async{
-    try{
-      await _db.collection('users').doc(user.values.first).set(user);
-    }on FirebaseAuth catch(e){
-      throw e.toString();
+  /// Function to save user record
+  Future<void> saveUserRecord(UserModel user) async {
+    try {
+      await _db.collection('Users').doc(user.id).set(user.toJson());
+    } on FirebaseException catch (e) {
+      throw ZFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw ZFormatException();
+    } on PlatformException catch (e) {
+      throw ZFormatException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
     }
-
   }
-
-
-
-
-
-
 }

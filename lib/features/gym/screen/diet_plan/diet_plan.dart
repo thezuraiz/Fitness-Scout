1a08c1224dc303f34dd78fa6235c46af9ce5f339
The Zuraiz <thezuraiz@gmail.com>
2024-10-20 17:09:37 +0500

    Fix Some bugs in Diet plan list

--- lib/features/gym/screen/diet_plan/diet_plan.dart
+++ lib/features/gym/screen/diet_plan/diet_plan.dart
@@ -32,12 +32,13 @@ class DietPlanScreen extends StatelessWidget {
             }),
 
             Padding(
-                padding: const EdgeInsets.symmetric(horizontal: ZSizes.md),
-                child: Obx(
-                  () => Column(
-                    children: [
-                      // Todo: Consumed Food
-                      if (dietplanController.dietTaken.isNotEmpty) ...[
+              padding: const EdgeInsets.symmetric(horizontal: ZSizes.md),
+              child: Column(
+                children: [
+                  // Todo: Consumed Food
+                  Obx(() {
+                    if (dietplanController.dietTaken.isNotEmpty) {
+                      return Column(children: [
                         const ZSectionHeading(
                           title: "Consumed Food",
                           showActionButton: false,
@@ -71,169 +72,215 @@ class DietPlanScreen extends StatelessWidget {
                           },
                         ),
                         const SizedBox(
-                          height: ZSizes.sm,
-                        ),
-                      ],
-
-                      // Todo: Break Fast
-                      if (dietplanController.dietPlan.value.breakfast !=
-                          null) ...[
-                        const ZSectionHeading(
-                          title: "BreakFast",
-                          showActionButton: false,
-                        ),
-                        const SizedBox(
-                          height: ZSizes.sm,
-                        ),
-                        ListView.separated(
-                          padding: const EdgeInsets.all(0),
-                          separatorBuilder: (context, index) => const SizedBox(
-                            height: ZSizes.sm,
-                          ),
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: dietplanController
-                              .dietPlan.value.breakfast!.length,
-                          itemBuilder: (context, index) {
-                            final breakfastItem = dietplanController
-                                .dietPlan.value.breakfast![index];
-                            return DietPlanTile(
-                              image: breakfastItem.imageUrl,
-                              textTitle: breakfastItem.name,
-                              protein: breakfastItem.protein.toString(),
-                              fat: breakfastItem.fat.toString(),
-                              carbs: breakfastItem.carbs.toString(),
-                              onTap: () {
-                                ZLogger.info('Diet Taken $breakfastItem');
-                                dietplanController.selectDiet(breakfastItem);
-                              },
-                            );
-                          },
-                        ),
-                        const SizedBox(
                           height: ZSizes.spaceBtwSections,
                         ),
-                      ],
+                      ]);
+                    } else {
+                      return const SizedBox();
+                    }
+                  }),
 
-                      // Todo: Lunch
-                      if (dietplanController.dietPlan.value.lunch != null) ...[
-                        const ZSectionHeading(
-                          title: "Lunch",
-                          showActionButton: false,
-                        ),
-                        const SizedBox(
-                          height: ZSizes.sm,
-                        ),
-                        ListView.separated(
-                          padding: const EdgeInsets.all(0),
-                          separatorBuilder: (context, index) => const SizedBox(
+                  // Todo: Break Fast
+                  Obx(() {
+                    final breakfast =
+                        dietplanController.dietPlan.value.breakfast;
+                    if (breakfast != null &&
+                        !breakfast.any((item) =>
+                            dietplanController.dietTaken.contains(item))) {
+                      return Column(
+                        children: [
+                          const ZSectionHeading(
+                            title: "BreakFast",
+                            showActionButton: false,
+                          ),
+                          const SizedBox(
                             height: ZSizes.sm,
                           ),
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount:
-                              dietplanController.dietPlan.value.lunch!.length,
-                          itemBuilder: (context, index) {
-                            final breakfastItem =
-                                dietplanController.dietPlan.value.lunch![index];
-                            return DietPlanTile(
-                              image: breakfastItem.imageUrl,
-                              textTitle: breakfastItem.name,
-                              protein: breakfastItem.protein.toString(),
-                              fat: breakfastItem.fat.toString(),
-                              carbs: breakfastItem.carbs.toString(),
-                              onTap: () {
-                                ZLogger.info('Diet Taken $breakfastItem');
-                                dietplanController.selectDiet(breakfastItem);
-                              },
-                            );
-                          },
-                        ),
-                        const SizedBox(
-                          height: ZSizes.spaceBtwSections,
-                        ),
-                      ],
+                          ListView.separated(
+                            padding: const EdgeInsets.all(0),
+                            separatorBuilder: (context, index) =>
+                                const SizedBox(
+                              height: ZSizes.sm,
+                            ),
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: breakfast.length,
+                            itemBuilder: (context, index) {
+                              final breakfastItem = breakfast[index];
+                              return DietPlanTile(
+                                image: breakfastItem.imageUrl,
+                                textTitle: breakfastItem.name,
+                                protein: breakfastItem.protein.toString(),
+                                fat: breakfastItem.fat.toString(),
+                                carbs: breakfastItem.carbs.toString(),
+                                onTap: () {
+                                  ZLogger.info('Diet Taken $breakfastItem');
+                                  dietplanController.selectDiet(breakfastItem);
+                                },
+                              );
+                            },
+                          ),
+                          const SizedBox(
+                            height: ZSizes.spaceBtwSections,
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const SizedBox();
+                    }
+                  }),
 
-                      // Todo: Snacks
-                      if (dietplanController.dietPlan.value.snacks != null) ...[
-                        const ZSectionHeading(
-                          title: "Snacks",
-                          showActionButton: false,
-                        ),
-                        const SizedBox(
-                          height: ZSizes.sm,
-                        ),
-                        ListView.separated(
-                          padding: const EdgeInsets.all(0),
-                          separatorBuilder: (context, index) => const SizedBox(
+                  // Todo: Lunch
+                  Obx(() {
+                    final lunch = dietplanController.dietPlan.value.lunch;
+                    if (lunch != null &&
+                        !lunch.any((item) =>
+                            dietplanController.dietTaken.contains(item))) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const ZSectionHeading(
+                            title: "Lunch",
+                            showActionButton: false,
+                          ),
+                          const SizedBox(
                             height: ZSizes.sm,
                           ),
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount:
-                              dietplanController.dietPlan.value.snacks!.length,
-                          itemBuilder: (context, index) {
-                            final breakfastItem = dietplanController
-                                .dietPlan.value.snacks![index];
-                            return DietPlanTile(
-                              image: breakfastItem.imageUrl,
-                              textTitle: breakfastItem.name,
-                              protein: breakfastItem.protein.toString(),
-                              fat: breakfastItem.fat.toString(),
-                              carbs: breakfastItem.carbs.toString(),
-                              onTap: () {
-                                ZLogger.info('Diet Taken $breakfastItem');
-                                dietplanController.selectDiet(breakfastItem);
-                              },
-                            );
-                          },
-                        ),
-                        const SizedBox(
-                          height: ZSizes.spaceBtwSections,
-                        ),
-                      ],
+                          ListView.separated(
+                            padding: const EdgeInsets.all(0),
+                            separatorBuilder: (context, index) =>
+                                const SizedBox(
+                              height: ZSizes.sm,
+                            ),
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: lunch.length,
+                            itemBuilder: (context, index) {
+                              final lunchItem = lunch[index];
+                              return DietPlanTile(
+                                image: lunchItem.imageUrl,
+                                textTitle: lunchItem.name,
+                                protein: lunchItem.protein.toString(),
+                                fat: lunchItem.fat.toString(),
+                                carbs: lunchItem.carbs.toString(),
+                                onTap: () {
+                                  ZLogger.info('Diet Taken $lunchItem');
+                                  dietplanController.selectDiet(lunchItem);
+                                },
+                              );
+                            },
+                          ),
+                          const SizedBox(
+                            height: ZSizes.spaceBtwSections,
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const SizedBox(); // Return an empty widget if the condition is not met.
+                    }
+                  }),
 
-                      // Todo: Dinner
-                      if (dietplanController.dietPlan.value.dinner != null) ...[
-                        const ZSectionHeading(
-                          title: "Dinner",
-                          showActionButton: false,
-                        ),
-                        const SizedBox(
-                          height: ZSizes.sm,
-                        ),
-                        ListView.separated(
-                          padding: const EdgeInsets.all(0),
-                          separatorBuilder: (context, index) => const SizedBox(
+                  // Todo: Snacks
+                  Obx(() {
+                    final snacks = dietplanController.dietPlan.value.snacks;
+                    if (snacks != null &&
+                        !snacks.any((item) =>
+                            dietplanController.dietTaken.contains(item))) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const ZSectionHeading(
+                            title: "Snacks",
+                            showActionButton: false,
+                          ),
+                          const SizedBox(
                             height: ZSizes.sm,
                           ),
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount:
-                              dietplanController.dietPlan.value.dinner!.length,
-                          itemBuilder: (context, index) {
-                            final breakfastItem = dietplanController
-                                .dietPlan.value.dinner![index];
-                            return DietPlanTile(
-                              image: breakfastItem.imageUrl,
-                              textTitle: breakfastItem.name,
-                              protein: breakfastItem.protein.toString(),
-                              fat: breakfastItem.fat.toString(),
-                              carbs: breakfastItem.carbs.toString(),
-                              onTap: () {
-                                ZLogger.info('Diet Taken $breakfastItem');
-                                dietplanController.selectDiet(breakfastItem);
-                              },
-                            );
-                          },
-                        ),
-                        const SizedBox(
-                          height: ZSizes.spaceBtwSections,
-                        )
-                      ],
-                    ],
-                  ),
-                )),
+                          ListView.separated(
+                            padding: const EdgeInsets.all(0),
+                            separatorBuilder: (context, index) =>
+                                const SizedBox(
+                              height: ZSizes.sm,
+                            ),
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: snacks.length,
+                            itemBuilder: (context, index) {
+                              return DietPlanTile(
+                                image: snacks[index].imageUrl,
+                                textTitle: snacks[index].name,
+                                protein: snacks[index].protein.toString(),
+                                fat: snacks[index].fat.toString(),
+                                carbs: snacks[index].carbs.toString(),
+                                onTap: () {
+                                  ZLogger.info(
+                                      'Diet Taken ${snacks[index].name}');
+                                  dietplanController.selectDiet(snacks[index]);
+                                },
+                              );
+                            },
+                          ),
+                          const SizedBox(
+                            height: ZSizes.spaceBtwSections,
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const SizedBox(); // Return an empty widget if the condition is not met.
+                    }
+                  }),
+
+                  // Todo: Dinner
+                  Obx(
+                    () {
+                      final dinner = dietplanController.dietPlan.value.dinner;
+                      if (dinner != null &&
+                          !dinner.any((item) =>
+                              dietplanController.dietTaken.contains(item))) {
+                        return Column(children: [
+                          const ZSectionHeading(
+                            title: "Dinner",
+                            showActionButton: false,
+                          ),
+                          const SizedBox(
+                            height: ZSizes.sm,
+                          ),
+                          ListView.separated(
+                            padding: const EdgeInsets.all(0),
+                            separatorBuilder: (context, index) =>
+                                const SizedBox(
+                              height: ZSizes.sm,
+                            ),
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: dinner.length,
+                            itemBuilder: (context, index) {
+                              final breakfastItem = dinner[index];
+                              return DietPlanTile(
+                                image: breakfastItem.imageUrl,
+                                textTitle: breakfastItem.name,
+                                protein: breakfastItem.protein.toString(),
+                                fat: breakfastItem.fat.toString(),
+                                carbs: breakfastItem.carbs.toString(),
+                                onTap: () {
+                                  ZLogger.info('Diet Taken $breakfastItem');
+                                  dietplanController.selectDiet(breakfastItem);
+                                },
+                              );
+                            },
+                          ),
+                          const SizedBox(
+                            height: ZSizes.spaceBtwSections,
+                          )
+                        ]);
+                      } else {
+                        return const SizedBox();
+                      }
+                    },
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
